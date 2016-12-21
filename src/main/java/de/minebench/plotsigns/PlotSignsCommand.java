255d442c158b1121572d42882d69335f7fd3c67f
Max Lee <mail@moep.tv>
2016-12-21 23:09:00 +0100

    Add some legacy commands back in

--- src/main/java/de/minebench/plotsigns/PlotSignsCommand.java
+++ src/main/java/de/minebench/plotsigns/PlotSignsCommand.java
@@ -16,10 +16,14 @@ package de.minebench.plotsigns;
  * along with this program. If not, see <http://mozilla.org/MPL/2.0/>.
  */
 
+import com.sk89q.worldguard.protection.flags.DefaultFlag;
+import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class PlotSignsCommand implements CommandExecutor {
     private final PlotSigns plugin;
@@ -30,13 +34,82 @@ public class PlotSignsCommand implements CommandExecutor {
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (args.length > 0) {
+
             if ("reload".equalsIgnoreCase(args[0]) && sender.hasPermission("plotsigns.command.reload")) {
                 plugin.loadConfig();
                 sender.sendMessage(ChatColor.YELLOW + "Config reloaded!");
                 return true;
+
+            } else if ("sell".equalsIgnoreCase(args[0]) || "verkaufen".equalsIgnoreCase(args[0]) && sender.hasPermission("plotsigns.command.sell")) {
+                // legacy sub command, you can write the signs directly
+                if (args.length > 2) {
+                    ProtectedRegion region = getRegion(sender, args[1]);
+
+                    if (region == null) {
+                        sender.sendMessage(plugin.getLang("create-sign.unknown-region", "region", args[1]));
+                        return true;
+                    }
+
+                    if (sender instanceof Player && !region.getOwners().contains(((Player) sender).getUniqueId()) && !sender.hasPermission("plotsigns.command.sell.others")) {
+                        sender.sendMessage(plugin.getLang("create-sign.doesnt-own-plot"));
+                        return true;
+                    }
+
+                    try {
+                        plugin.makeRegionBuyable(region, Double.parseDouble(args[2]), region.getFlag(PlotSigns.BUY_PERM_FLAG));
+                    } catch (NumberFormatException e) {
+                        sender.sendMessage(plugin.getLang("create-sign.malformed-price", "input", args[2]));
+                    } catch (IllegalArgumentException e) {
+                        sender.sendMessage(ChatColor.RED + "Error while trying to make the region buyable! " + e.getMessage());
+                    }
+                } else {
+                    sender.sendMessage(ChatColor.RED + "Usage: /" + label + " " + args[0] + " <region> <price>");
+                }
+                return true;
+
+            } else if ("permission".equalsIgnoreCase(args[0]) && sender.hasPermission("plotsigns.command.permission")) {
+                // legacy sub command, you can write the signs directly
+                if (args.length > 2) {
+                    ProtectedRegion region = getRegion(sender, args[1]);
+
+                    if (region == null) {
+                        sender.sendMessage(plugin.getLang("create-sign.unknown-region", "region", args[1]));
+                        return true;
+                    }
+
+                    if (sender instanceof Player && !region.getOwners().contains(((Player) sender).getUniqueId()) && !sender.hasPermission("plotsigns.command.permission.others")) {
+                        sender.sendMessage(plugin.getLang("create-sign.doesnt-own-plot"));
+                        return true;
+                    }
+
+                    try {
+                        plugin.makeRegionBuyable(region, region.getFlag(DefaultFlag.PRICE), args[2]);
+                    } catch (IllegalArgumentException e) {
+                        sender.sendMessage(ChatColor.RED + "Error while trying to make the region buyable! " + e.getMessage());
+                    }
+                } else {
+                    sender.sendMessage(ChatColor.RED + "Usage: /" + label + " " + args[0] + " <region> <permission>");
+                }
+                return true;
+
             }
         }
         return false;
+    }
+
+    private ProtectedRegion getRegion(CommandSender sender, String id) {
+        ProtectedRegion region = null;
+        if (sender instanceof Player) {
+            region = plugin.getWorldGuard().getRegionManager(((Player) sender).getWorld()).getRegion(id);
+        } else {
+            for (RegionManager rm : plugin.getWorldGuard().getRegionContainer().getLoaded()) {
+                region = rm.getRegion(id);
+                if (region != null) {
+                    break;
+                }
+            }
+        }
+        return region;
     }
 
 }
